71dde1369d572cf31a8073f61bec2658abab220c
김초희 <qwerpoiu1316@naver.com>
2020-09-01 21:34:59 +0900

    Add Codes : 헤더 메뉴 추가

--- opuscout/src/App.js
+++ opuscout/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header>{this.props.children}</Header>
       </div>
     );
   }
--- opuscout/src/components/Header.js
+++ opuscout/src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './Header.css'
 
+const MenuItem = ({active, children, to}) => {
+    return(
+        <div className="menu-item">
+            {children}
+        </div>
+    )    
+}
+
 const Header = () => {
     return (
         <div>
             <div className="header-container">
                 <div className="web-logo">로고</div>
                 <div className="web-name">OPUSCOUT</div>
-
+                <div className="menu">
+                    <MenuItem>아이템 발굴</MenuItem>
+                    <MenuItem>키워드 탐색</MenuItem>
+                    <MenuItem>카테고리 분석</MenuItem>
+                    <MenuItem>인기 아이템</MenuItem>
+                </div>
             </div>
         </div>
     )
