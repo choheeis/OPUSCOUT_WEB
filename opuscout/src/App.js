07c26517e4414b4dd18cf2420ffcf4c74bda7311
김초희 <qwerpoiu1316@naver.com>
2020-09-01 16:04:04 +0900

    Add Codes : 헤더 레이아웃 구현

--- opuscout/src/App.js
+++ opuscout/src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Nav from './components/Nav';
+import React, {Component} from 'react';
+import Header from './components/Header';
 
-
-function App() {
-  return (
-    <div className="App">
-      <Nav></Nav>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ opuscout/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className="header-container">
+                <div className="web-logo">로고</div>
+                <div className="web-name">OPUSCOUT</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Header;
