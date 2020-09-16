4a9af5bf5334b69846631601635dd20faf534be6
김초희 <qwerpoiu1316@naver.com>
2020-09-16 21:23:07 +0900

    Add Codes : 메인 페이지 Introduction UI 구현 완료

--- opuscout/src/components/home/Feature.js
+++ opuscout/src/components/home/Feature.js
@@ -1,27 +1,73 @@
 import React from 'react';
 import styled from 'styled-components';
+import ItemImg from '../../resource/item.svg'
+import KeyWordImg from '../../resource/keyword.svg'
+import CategoryImg from '../../resource/category.svg'
+import HotImg from '../../resource/hot.svg'
 
 const FeatureStyle = styled.div`
-    background: #D3E0FF4D;
     width: 300px;
     flex: 1 1 auto;
     margin: 50px;
+
+    &:hover {
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.07);
+    }
 `;
 
 const ImgSection = styled.div`
-    
+    background: #D3E0FF4D;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .img {
+        width: 150px;
+        height: 150px;
+    }
 `;
 
 const Title = styled.div`
+    margin-top: 40px;
+    text-align: center;
     color: #000000;
-    font-size: 30px;
+    font-size: 20px;
+    font-weight: bold;
+`;
+
+const Description = styled.div`
+    display: flex;
+    margin: 0 auto;
+    margin-top: 15px;
+    text-align: center;
+    color: #333333;
+    font-size: 15px;
+    width: 200px;
 `;
 
 function Feature({ name, description, img}) {
+    var imgSrc = '';
+    if (img === "item") {
+        imgSrc = ItemImg
+    }
+    if (img === "keyword") {
+        imgSrc = KeyWordImg
+    }
+    if (img === "category") {
+        imgSrc = CategoryImg
+    }
+    if (img === "hot") {
+        imgSrc = HotImg
+    }
     return(
         <>
             <FeatureStyle>
-                dfadfds
+                <ImgSection>
+                    <img className="img" src={imgSrc} alt="아이템 발굴 아이콘"></img> 
+                </ImgSection>
+                <Title>{name}</Title>
+                <Description>{description}</Description>
             </FeatureStyle>
         </>
     )
--- opuscout/src/containers/Home.js
+++ opuscout/src/containers/Home.js
@@ -35,10 +35,13 @@ function Home() {
                 <div className="container">
                     <div className="introduction">Introduction</div>
                     <RowStyle>
-                        <Feature></Feature>
-                        <Feature></Feature>
+                        <Feature name="아이템 발굴" description="온라인 오픈마켓 내에서 경쟁력 있는 판매 아이템을 찾아보세요!" img="item"></Feature>
+                        <Feature name="키워드 탐색" description="판매 아이템에 적합한 키워드들을 찾아보세요!" img="keyword"></Feature>
                     </RowStyle>
-                    <RowStyle></RowStyle>
+                    <RowStyle>
+                        <Feature name="카테고리 분석" description="다양한 필터를 통해 온라인 오픈마켓에서 사용되는 카테고리를 분석해보세요!" img="category"></Feature>
+                        <Feature name="인기 아이템" description="매주 업데이트 되는 인기 아이템을 확인해보세요!" img="hot"></Feature>
+                    </RowStyle>
                 </div>
             </IntroductionSection>
         </>
