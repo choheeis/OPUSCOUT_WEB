134feb36badc2bf0bdd156f382c25cfe7fa2feff
김초희 <qwerpoiu1316@naver.com>
2020-09-16 17:21:29 +0900

    Add Codes : 홈 화면 구현중

---
+++ opuscout/src/components/home/Feature.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FeatureStyle = styled.div`
+    background: #D3E0FF4D;
+    width: 300px;
+    flex: 1 1 auto;
+    margin: 50px;
+`;
+
+const ImgSection = styled.div`
+    
+`;
+
+const Title = styled.div`
+    color: #000000;
+    font-size: 30px;
+`;
+
+function Feature({ name, description, img}) {
+    return(
+        <>
+            <FeatureStyle>
+                dfadfds
+            </FeatureStyle>
+        </>
+    )
+}
+
+export default Feature;
--- opuscout/src/containers/Home.js
+++ opuscout/src/containers/Home.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import Feature from '../components/home/Feature';
+
+const ContentSection = styled.div`
+    display: flex;
+    background: #E4E4E4;
+    height: 500px;
+`;
+
+const IntroductionSection = styled.div`
+    .container {
+        width: 60%;
+        margin: 0 auto;
+    }
+
+    .introduction {
+        margin-top: 100px;
+        margin-bottom: 80px;
+        font-size: 30px;
+        text-align: center;
+    }
+`;
+
+const RowStyle = styled.div`
+    display: flex;
+    height: 500px;
+`;
 
 function Home() {
     return (
-        <div>
-            <h1>메인 페이지 만들어야함 디자인도 해야함</h1>
-        </div>
+        <>
+            <ContentSection>djkdjk</ContentSection>
+            <IntroductionSection>
+                <div className="container">
+                    <div className="introduction">Introduction</div>
+                    <RowStyle>
+                        <Feature></Feature>
+                        <Feature></Feature>
+                    </RowStyle>
+                    <RowStyle></RowStyle>
+                </div>
+            </IntroductionSection>
+        </>
     );
 }
 
