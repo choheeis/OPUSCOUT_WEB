0b1bcf7d8b4278738826488d5961a3a0822c7f90
Kimchohee <chchgml10@gmail.com>
2020-12-08 23:16:19 +0900

    깃랩 코드 정리

--- Chrome Extension/PageFontStyle/content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.sendMessage({ todo: 'showPageAction' });
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.todo == 'changeColor') {
-        let addColor = '#' + request.clickedColor;
-        $('.api').css('color', addColor);
-    }
-});
